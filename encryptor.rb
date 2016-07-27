dfa5303c139bb26c6f003eb526ae78ec74888071
Noah Berman <noah@noahberman.org>
2016-07-27 11:30:57 -0500

    adds initial ROT-13 encryptor

---
+++ encryptor.rb
@@ -0,0 +1,26 @@
+class Encryptor
+  def cipher                            # letter hash for ROT-13 cipher
+    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
+     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
+     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
+     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
+     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
+     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
+     'y' => 'l', 'z' => 'm'}
+   end
+
+   def encrypt_letter(letter)           # just encrypts one letter
+     lowercase_letter = letter.downcase
+     cipher[lowercase_letter]
+   end
+
+   def encrypt(string)                  # encrypts a string
+     letters = string.split("")
+     results = []
+     letters.each do |letter|
+       encrypted_letter = encrypt_letter(letter)
+       results.push(encrypted_letter)
+   end
+      results.join                     # joins encrypted array as a string
+end
+end
