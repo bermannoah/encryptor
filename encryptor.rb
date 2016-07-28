560ef3ce6528a12f1c3857136da1ccc2ed0f9a47
Noah Berman <noah@noahberman.org>
2016-07-28 15:12:11 -0500

    adds file decryptor

--- encryptor.rb
+++ encryptor.rb
@@ -16,7 +16,7 @@ class Encryptor
        encrypted_letter = encrypt_letter(letter, rotation)
    end
      results.join                     # joins encrypted array as a string
-end
+  end
 
   def decrypt(string, rotation)                 # decrypts an encrypted string using encrypt
     x = (0 - rotation).to_i
@@ -25,13 +25,24 @@ end
       decrypted_letter = encrypt_letter(letter, x)
     end
     decrypted_results.join            # outputs decrypted string
-end
-    def encrypt_file(filename, rotation)
+    end
+
+
+  def encrypt_file(filename, rotation)
       input = File.open(filename, "r")  # opens file
       clear_text = input.read           # reads file into cleartext
       cipher_text = encrypt(clear_text, rotation) # encrypts clear text with rotation parameter
       output = File.open(filename + ".encrypted", "w") # creates file for encrypted text
       output.write(cipher_text) # writes cipher text to new file
       output.close   # closes file
-end
+  end
+
+  def decrypt_file(filename, rotation)
+      input = File.open(filename, "r")  # opens file
+      code_language = input.read    # reads gibberish
+      not_code_language = decrypt(code_language, rotation)  # decrypts gibberish
+      what_it_says = File.open(filename.gsub("encrypted", "decrypted"), "w") # creates a new file and changes encrypted to decrypted
+      what_it_says.write(not_code_language) # writes decrypted info to new file
+      what_it_says.close  # closes that file
+    end
 end
