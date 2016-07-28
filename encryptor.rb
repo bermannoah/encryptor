2181bf2f8f1f2d310eb10bd0789072eb13d04a10
Noah Berman <noah@noahberman.org>
2016-07-28 16:42:52 -0500

    adds ability to add passwords but actually using them is totally broken

--- encryptor.rb
+++ encryptor.rb
@@ -32,7 +32,7 @@ class Encryptor
       input = File.open(filename, "r")  # opens file
       clear_text = input.read           # reads file into cleartext
       cipher_text = encrypt(clear_text, rotation) # encrypts clear text with rotation parameter
-      output = File.open(filename + ".encrypted", "w") # creates file for encrypted text
+      output = File.open(filename, "w") # creates file for encrypted text
       output.write(cipher_text) # writes cipher text to new file
       output.close   # closes file
   end
@@ -41,7 +41,7 @@ class Encryptor
       input = File.open(filename, "r")  # opens file
       code_language = input.read    # reads gibberish
       not_code_language = decrypt(code_language, rotation)  # decrypts gibberish
-      what_it_says = File.open(filename.gsub("encrypted", "decrypted"), "w") # creates a new file and changes encrypted to decrypted
+      what_it_says = File.open(filename, "w") # creates a new file and changes encrypted to decrypted
       what_it_says.write(not_code_language) # writes decrypted info to new file
       what_it_says.close  # closes that file
     end
@@ -56,45 +56,47 @@ class Encryptor
     end
   end
 
-password_file = File.open("encryptor_password.txt", "r")
-password_file.read
+password = File.open("encryptor_password.txt", "r")
+password.read
 
-if password_file == "no password\n"
-  puts "Add a password to use this application > "
-    password = gets.chomp
-      e = Encryptor.new
-        saved_password = e.encrypt(password, 2048)
-        security = File.open(password_file, "w")
-        security.write(saved_password)
+
+  if File.zero?("encryptor_password.txt")         # checks to see if the password file is empty
+    puts "Add a password to use this application > "
+      password_new = gets.chomp
+        security = File.open("encryptor_password.txt", "w")
+        security.write(password_new)
         security.close
-else
-  puts "Enter password to login > "
-    login = gets.chomp
-    e = Encryptor.new
-        checker = decrypt_file(password_file, 2048)
-    if checker != login
-      puts "Your password was entered incorrectly. Try again."
-    else
-      puts "Enter 1 to encrypt or 2 to decrypt."          # code to make the program more user friendly
-        selection = gets.chomp.to_i
+        e = Encryptor.new
+        e.encrypt_file("encryptor_password.txt", 2048)
+  else
+    puts "Enter password to login > "
+      login = gets.chomp
+        e = Encryptor.new
+        e.decrypt_file("encryptor_password.txt", 2048)
+        checker = File.open("encryptor_password.txt", "r")
+          if login == checker
+            puts "Enter 1 to encrypt or 2 to decrypt."          # code to make the program more user friendly
+              selection = gets.chomp.to_i
 
-        if selection == 1
-          puts "Enter text to be encrypted > "
-            string = gets.chomp
-          puts "Enter rotation number > "
-            rotation = gets.chomp.to_i
-          e = Encryptor.new
-          puts e.encrypt(string, rotation)
-        elsif selection == 2
-          puts "Enter text to be decrypted > "
-            string = gets.chomp
-          puts "Enter rotation number > "
-            rotation = gets.chomp.to_i
-          e = Encryptor.new
-          puts e.decrypt(string, rotation)
-        else
-          puts "Please enter 1 or 2."
+              if selection == 1
+                puts "Enter text to be encrypted > "
+                  string = gets.chomp
+                puts "Enter rotation number > "
+                  rotation = gets.chomp.to_i
+                e = Encryptor.new
+                puts e.encrypt(string, rotation)
+              elsif selection == 2
+                puts "Enter text to be decrypted > "
+                  string = gets.chomp
+                puts "Enter rotation number > "
+                  rotation = gets.chomp.to_i
+                e = Encryptor.new
+                puts e.decrypt(string, rotation)
+              else
+                puts "Please enter 1 or 2."
+            end
+          else
+            puts "Your password was entered incorrectly. Try again."
+          end
+        end
       end
-    end
-end
-end
